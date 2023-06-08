8785339062146ff5250a85e5e48024d8ef662ef0
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-06-08 20:57:07 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WafflesController } from './waffles/waffles.controller';
-import { WafflesService } from './waffles/waffles.service';
+import { WafflesController } from './dataForFront/waffles.controller';
+import { WafflesService } from './dataForFront/waffles.service';
 import { CustomerCartController } from './customer-cart/customer-cart.controller';
 import { CustomerCartService } from './customer-cart/customer-cart.service';
 
