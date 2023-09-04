990353e1d4eceb031e853780f588884af87eaedd
Fernando Frias <fernandofrias@tdf.edu.ar>
2023-09-04 07:21:51 -0300

    modificacion en el service de cart para empezar a darle funcionalidad a los botones de table

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -7,17 +7,41 @@ export class CartService {
   public getCartOrder(): any {
     return this.order;
   }
+
   public addCartOrder(body): string {
-    this.order.push(body);
-    return `The following article was added to the order:
-  id : ${body.id}
-  name : ${body.name}
-  description : ${body.description}
-  imgUrl : ${body.imgUrl}
-  price : ${body.price}
-  quantity : ${body.quantity}
-  `;
+
+    let existe = false;
+    let posicion;
+
+    for (let i = 0; i < this.order.length; i++) {
+      if (this.order[i].id == body.id) {
+        console.log("es igual");
+        existe = true;
+        posicion = i;
+      };
+    }
+    
+      if ( existe === true){
+        this.order[posicion].quantity += 1; 
+        console.log(this.order[posicion]);
+        console.log(this.order);
+
+      }else{
+
+        this.order.push(body);
+        return `The following article was added to the order:
+          id : ${body.id}
+          name : ${body.name}
+          description : ${body.description}
+          imgUrl : ${body.imgUrl}
+          price : ${body.price}
+          quantity : ${body.quantity}
+          `;
+      }
   }
+
+
+
   public deleteCartOrder(): any {
     return this.order.splice(0, this.order.length);
   }
