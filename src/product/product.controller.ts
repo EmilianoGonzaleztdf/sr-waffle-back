a7cc562371b4c88e9349bdc1ad96c0e5c57603b1
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-11-07 19:09:13 -0300

    product casi funcional. falta pasar FK_categoria

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Controller('products')
 export class ProductController {
@@ -20,14 +21,14 @@ export class ProductController {
     return this.productService.searchProductsByKeyword(keyword);
   } ;
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+  @Patch('/update/:id')
+  async update (@Body() product:Product, @Param('id') id : number) : Promise<String>{
+    return await this.productService.update(id, product);
   }
 
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  @Delete('/delete/:id')
+  async remove (@Param('id') id : number) : Promise<boolean>{
+    return this.productService.remove(id);
   }
   @Post('/createProduct')
   async create(@Body() createProductDto: CreateProductDto) {
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { Repository } from 'typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
 
 @Injectable()
 export class ProductService {
@@ -28,13 +27,31 @@ export class ProductService {
       .getMany();
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, createProduct: CreateProductDto) {
+    const criteria : FindOneOptions = { where : { id_product : id}};
+    let product : Product = await this.productRepository.findOne(criteria);
+    if (!product) {
+      throw new Error('no se pudo encontrar el producto a modificar');
+    } else {
+      product.setBar_Code(createProduct.bar_code);
+      product.setDescription(createProduct.description);
+      product.setImgURL(createProduct.imgURL);
+      product.setName(createProduct.name);
+      product.setPrice(createProduct.price);
+      product = await this.productRepository.save(product);
+      return ('se cambio el producto')
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const criteria: FindOneOptions = { where: { id_product: id } };
+    let product : Product = await this.productRepository.findOne(criteria);
+      if(product){
+        await this.productRepository.remove(product);
+        return true;
+      } else throw new Error('no se encontro el producto a eliminar');
   }
+
   async create(createProductDto: CreateProductDto): Promise<Product> {
     const { bar_code, name, description, imgURL, price } = createProductDto;
 
