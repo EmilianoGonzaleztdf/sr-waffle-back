c897068acc74e16ff9a04f755a1436bda4539010
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-11-11 18:12:40 -0300

    conexion a db online

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,11 +28,11 @@ import { HistorySaleModule } from './history_sale/history_sale.module';
 @Module({
   imports: [    TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',
+    host: 'b1ycqaa9ezvmxmk0b3yt-mysql.services.clever-cloud.com',
     port: 3306,
-    username: 'root',
-    password: 'emiliano1',
-    database: 'tp_anual',
+    username: 'uunm7jwef64aq6my',
+    password: '4kQdqDhA8YmuDIG4DsVk',
+    database: 'b1ycqaa9ezvmxmk0b3yt',
     entities: [__dirname + '/**/**/**.entity{.ts,.js}'],
     synchronize: true, //modo desarrollador.
   }), CategoryModule, ProductModule, RoleModule, PersonModule, UserModule, OrderModule, StatusModule, SaleModule, HistorySaleModule,],
