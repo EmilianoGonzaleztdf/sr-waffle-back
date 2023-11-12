171be56c6c11314e588dbbad501f603ad070b78b
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-11-11 23:38:28 -0300

    update

--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -3,9 +3,10 @@ import { PersonService } from './person.service';
 import { PersonController } from './person.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Person } from './entities/person.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Person])],
+  imports: [TypeOrmModule.forFeature([Person, User])],
   controllers: [PersonController],
   providers: [PersonService]
 })
