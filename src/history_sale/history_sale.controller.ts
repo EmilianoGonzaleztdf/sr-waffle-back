1ea425992bf1854fcb411010a2ff8929851ba35a
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-11-19 14:53:48 -0300

    update

--- src/history_sale/history_sale.controller.ts
+++ src/history_sale/history_sale.controller.ts
@@ -1,34 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { HistorySaleService } from './history_sale.service';
-import { CreateHistorySaleDto } from './dto/create-history_sale.dto';
-import { UpdateHistorySaleDto } from './dto/update-history_sale.dto';
+import { HistorySale } from './entities/history_sale.entity';
 
 @Controller('history-sale')
 export class HistorySaleController {
   constructor(private readonly historySaleService: HistorySaleService) {}
 
-  @Post()
-  create(@Body() createHistorySaleDto: CreateHistorySaleDto) {
-    return this.historySaleService.create(createHistorySaleDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.historySaleService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.historySaleService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateHistorySaleDto: UpdateHistorySaleDto) {
-    return this.historySaleService.update(+id, updateHistorySaleDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.historySaleService.remove(+id);
-  }
 }
